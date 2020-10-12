31ec1465298988ca8436a10c008a853c9583e261
DeAndre Bethell <54558868+Dbethell001@users.noreply.github.com>
2020-10-12 00:54:27 -0500

    Update DartGame.java
    This is the updated Pi Approximator.

--- DartGame.java
+++ DartGame.java
@@ -2,7 +2,6 @@
 //DeAndre Bethell
 //October 1st 2020
 
-import java.util.*;
 import java.lang.Math;
 
 public class DartGame {
@@ -10,28 +9,32 @@ public class DartGame {
 	 public DartGame() {
 	 }
 	 
-	 public HashMap<Double, Double> randomGen(int size) {
-	  int counter = size;
-	  HashMap<Double, Double> pairs = new HashMap<Double, Double>();
-	  while(counter > 0) {
-	  Double a = Math.random() * 8.875;
-	  Double b = Math.random() * 8.875;
-	  pairs.put(a, b);
-	  counter --;
-	  }
-	   return pairs;
-	  }
+	 public Double [][] randomGen(int size){
+		  int count = size;
+		  Double[][] pairs = new Double[size][size * 2];
+		  for(int i = 0; i < count; i++){
+			 for(int j = 0; j < count; j++) {
+				  Double x = Math.random() * 0.5;
+				  Double y = Math.random() * 0.5;
+				  pairs[i][j] = x;
+				  j++;
+				  pairs[i][j] = y;
+			 }
+		  }
+		  return pairs;
+	 }
 	  
 	 
-	 public Double[] centerDistance(HashMap<Double, Double> pairs) {
-	  Set<Double> keys = pairs.keySet();
-	  Double[] keyArray = keys.toArray(new Double[keys.size()]);
-	  Double[] distance = new Double[keys.size()];
-	  for(int i =0; i <= pairs.size()-1; i++) {
-	   Double a = keyArray[i];
-	   Double b = pairs.get(a);
-	   Double c = Math.sqrt((a*a)+(b*b));
-	   distance [i] = c;
+	 public Double[] centerDistance(Double [][] pairs) {
+	  Double[] distance = new Double[pairs.length];
+	  for(int i =0; i <= pairs.length-1; i++) {
+		  for(int j = 0; j < (pairs.length-1) * 2; j++) {
+			   Double x = pairs[i][j];
+			   j++;
+			   Double y = pairs[i][j];
+			   Double z = Math.sqrt((x*x)+(y*y));
+			   distance [i] = z;
+		  }
 	  }
 	  
 	  return distance;
@@ -40,7 +43,7 @@ public class DartGame {
 	  public double radiusCount(Double[] distance) {
 	  double dartPoints = 0.0;
 	  for(int i = 0; i <= distance.length - 1; i++) {
-	   if(distance[i] < 8.875) {
+	   if(distance[i] < 0.5) {
 	    dartPoints ++;
 	   }
 	   else;
@@ -49,43 +52,34 @@ public class DartGame {
 	  return dartPoints;
 	 }
 	  public double standDev(Double[] distance) {
-		  double sum = 0;
-		  double max = 0;
+		  double sum = 0.0;
+		  double mean = 0.0;
 		  double min = distance[0];
 		  double sd = 0.0;
+		  
 		  for (int i = 0; i < distance.length; i++){
 		  sum = sum + distance[i];
 		  }
-		  double average = sum / distance.length;
+		  mean = sum / distance.length;
+		  
 		  for(int i = 0; i < distance.length; i++) {
-		  if(distance[i] > max) {
-		  max = distance[i];
-		  }
-		  }
-		  for (int i = 0; i <distance.length; i++) {
-		  if(distance[i] < min) {
-		  min = distance[i];
-		  }
-		  }
-		  for(int i = 0; i < distance.length; i++) {
-		  sd += Math.pow((distance[i] - average) / distance.length, 2);
+		  sd += Math.pow(distance[i] - mean, sd) *(distance[i] - mean) / distance.length;
 		  }
 		  double standDeviation = Math.sqrt(sd);
+		 System.out.println("Standard Deviation" + standDeviation);
 		  return standDeviation;
 		  } 
 // Main method
 	 public static void main(String args[]) {
 	  
 	  DartGame game = new DartGame();
-	  int dartGame = (int)(Math.random()* 20)+1;
+	  int dartGame = (int) ((Math.random()* 500)+1.0);
 	  System.out.println("Number of games: "  + dartGame);
-	  HashMap<Double, Double> pairs = game.randomGen(dartGame);
+	  Double [][] pairs = game.randomGen(dartGame);
 	  Double[] distance = game.centerDistance(pairs);
 	  double radCount = game.radiusCount(distance);
-	  double attempt = 4 * (radCount / (pairs.size()));
+	  double attempt = 4 * (radCount / pairs.length);
 	  System.out.println(attempt);
 	  
 	 }
-	}
-
-
+}
